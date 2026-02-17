8055291bef7460aeb92e93b655d1fdc942e1b94e
Giampaolo Tranchida <giampaolo@tranchida.ch>
2026-02-17 04:54:23 +0100

    Refactor BillResourceTest to improve readability and maintainability

--- src/test/java/ch/tranchida/sample/quarkus/BillResourceTest.java
+++ src/test/java/ch/tranchida/sample/quarkus/BillResourceTest.java
@@ -7,7 +7,9 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.anyOf;
+
+import org.hamcrest.Matchers;
+
 
 @QuarkusTest
 class BillResourceTest {
@@ -22,7 +24,7 @@ class BillResourceTest {
              .body("", hasSize(5))
              .body("[0].id", is(1))
              .body("[0].description", is("Bill 1"))
-             .body("[0].amount", anyOf(is(10), is(10.0)))
+             .body("[0].amount", Matchers.<Object>anyOf(is(10.0), is(10)))
              .body("[4].id", is(5))
              .body("[4].description", is("Bill 5"));
     }
